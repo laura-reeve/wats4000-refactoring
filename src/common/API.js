20a61c3946b45585c60bf7e79d0dc22a07710392
Laura Reeve <lauralee51@gmail.com>
2017-11-04 14:49:46 -0700

    Coding complete - needs comments.

---
+++ src/common/API.js
@@ -0,0 +1,16 @@
+// Base API configuration
+
+import axios from 'axios';
+
+export const API = axios.create ({
+    baseURL: '//api.openweathermap.org/data/2.5/'
+}) 
+
+API.interceptors.request.use(function (config) {
+    // Set common parameters on each request to the API
+    config.params.APPID = 'b3c73779bc0c4c2b403c79bc2f5e5e00';
+    config.params.units = 'imperial';
+    return config;
+}, function(error) {
+    return Promise.reject(error);
+}); 
--- src/components/Forecast.vue
+++ src/components/Forecast.vue
@@ -9,37 +9,22 @@
     <ul v-if="weatherData && errors.length===0" class="forecast">
       <li v-for="forecast in weatherData.list">
         <h3>{{ forecast.dt|formatDate }}</h3>
-        <!-- TODO: Make weather summary be in a child component. -->
-        <div v-for="weatherSummary in forecast.weather" class="weatherSummary">
-            <img v-bind:src="'http://openweathermap.org/img/w/' + weatherSummary.icon + '.png'" v-bind:alt="weatherSummary.main">
-            <br>
-            <b>{{ weatherSummary.main }}</b>
-        </div>
-        <!-- TODO: Make dl of weather data be in a child component. -->
-        <dl>
-            <dt>Humidity</dt>
-            <dd>{{ forecast.main.humidity }}%</dd>
-            <dt>High</dt>
-            <dd>{{ forecast.main.temp_max }}&deg;F</dd>
-            <dt>Low</dt>
-            <dd>{{ forecast.main.temp_min }}&deg;F</dd>
-        </dl>
+          <weather-summary v-bind:weatherData="forecast.weather"></weather-summary>
+          <weather-conditions v-bind:weatherData="forecast.main"></weather-conditions>
       </li>
     </ul>
-    <div v-else-if="errors.length > 0">
-      <h2>There was an error fetching weather data.</h2>
-      <ul class="errors">
-        <li v-for="error in errors">{{ error }}</li>
-      </ul>
-    </div>
     <div v-else>
       <h2>Loading...</h2>
     </div>
+    <error-list v-bind:errorList="errors"></error-list>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import {API} from '@/common/API';
+import WeatherSummary from '@/components/WeatherSummary';
+import WeatherConditions from '@/components/WeatherConditions';
+import ErrorList from '@/components/ErrorList';
 
 export default {
   name: 'Forecast',
@@ -51,14 +36,11 @@ export default {
     }
   },
   created () {
-    // TODO: Improve base config for API
-    axios.get('//api.openweathermap.org/data/2.5/forecast', {
-      params: {
-          id: this.$route.params.cityId,
-          units: 'imperial',
-          APPID: 'YOUR_APPID_HERE'
-      }
-    })
+    API.get('forecast', {
+        params: {
+          id: this.$route.params.cityId
+        }
+      })
     .then(response => {
       this.weatherData = response.data
     })
@@ -87,18 +69,18 @@ export default {
       }
       //let year = date.getFullYear();
       return `${ months[month] } ${ daynum } @ ${ hour }`;
+      }
+    },
+    components: {
+      'weather-summary': WeatherSummary,
+      'weather-conditions': WeatherConditions,
+      'error-list': ErrorList
     }
-  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.errors li {
-  color: red;
-  border: solid red 1px;
-  padding: 5px;
-}
 h1, h2 {
   font-weight: normal;
 }
@@ -119,29 +101,7 @@ li {
 a {
   color: #42b983;
 }
-.weatherSummary {
-  display: inline-block;
-  width: 100px;
-}
-dl {
-  padding: 5px;
-  background: #e8e8e8;
-}
-dt {
-  float: left;
-  clear: left;
-  width: 120px;
-  text-align: right;
-  font-weight: bold;
-  color: blue;
-}
-dd {
-  margin: 0 0 0 130px;
-  padding: 0 0 0.5em 0;
-}
-dt::after {
-  content: ":";
-}
+
 </style>
 
 
